8fa392452bbd60ba96570bf904d5d609f51f07c6
Shakti045 <122844750+Shakti045@users.noreply.github.com>
2023-02-06 14:09:04 +0530

    Update maximum profit from stocks with transaction fee.java

--- maximum profit from stocks with transaction fee.java
+++ maximum profit from stocks with transaction fee.java
@@ -1,3 +1,6 @@
+//only recursion
+
+
 public class Shakti {
     public static int maxprofit(int arr[],int tfee,int index,int option) {
         if(index==arr.length){
@@ -20,3 +23,47 @@ public class Shakti {
         System.out.println(maxprofit(arr,tfee,0,0));
     }
 }
+  
+
+
+//using memoization
+
+
+import java.util.Arrays;
+
+public class Shakti {
+    public static int maxprofit(int arr[],int tfee,int index,int option,int dp[][]) {
+        if(index==arr.length){
+            return 0;
+        }
+        if(dp[index][option]!=-1){
+            return dp[index][option];
+        }
+        if (option == 0) { //allowed to buy
+            int a = -arr[index] + maxprofit(arr, tfee, index + 1, 1,dp);
+            int b = maxprofit(arr, tfee, index + 1, 0,dp);
+            dp[index][option]=Math.max(a, b);
+            return dp[index][option];
+        } else {
+            //allowed to sell
+            int a = arr[index] - tfee + maxprofit(arr, tfee, index + 1, 0,dp);
+            int b = maxprofit(arr, tfee, index + 1, 1,dp);
+             dp[index][option]=Math.max(a, b);
+            return dp[index][option];
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={6,1,7,2,8,4};
+        int tfee=2;
+        int dp[][]=new int[arr.length][2];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        System.out.println(maxprofit(arr,tfee,0,0,dp));
+    }
+}
+
+
+
+//using tabulation
+
